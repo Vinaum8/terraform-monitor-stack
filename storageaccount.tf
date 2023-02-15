330b27de7f08e80abd39113a27ed5598b645c664
Kuno <gustavo.kuno@linx.com.br>
2023-02-14 21:48:28 -0300

    prometheus rules and examples in config

--- storageaccount.tf
+++ storageaccount.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "main" {
-    name = "rg-terraform-monitor-stack"
-    location = var.location
+  name     = "rg-terraform-monitor-stack"
+  location = var.location
 }
 
 resource "random_id" "storage_account" {
@@ -8,34 +8,40 @@ resource "random_id" "storage_account" {
 }
 
 resource "azurerm_storage_account" "main" {
-    name = "stg${lower(random_id.storage_account.hex)}"
-    account_tier = "Standard"
-    account_replication_type = "LRS"
-    location = azurerm_resource_group.main.location
-    resource_group_name = azurerm_resource_group.main.name
+  name                     = "stg${lower(random_id.storage_account.hex)}"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  location                 = azurerm_resource_group.main.location
+  resource_group_name      = azurerm_resource_group.main.name
 
-    depends_on = [
-      azurerm_resource_group.main,
-      random_id.storage_account
-    ]
+  depends_on = [
+    azurerm_resource_group.main,
+    random_id.storage_account
+  ]
 }
 
 resource "azurerm_storage_share" "stg_files_prometheus" {
   name                 = "prometheus"
   storage_account_name = azurerm_storage_account.main.name
-  quota = 10
+  quota                = 10
   depends_on           = [azurerm_storage_account.main]
 }
 
 resource "azurerm_storage_share_file" "prometheus_config_file" {
-  name = "prometheus.yml"
+  name             = "prometheus.yml"
   storage_share_id = azurerm_storage_share.stg_files_prometheus.id
-  source = "./prometheus/prometheus.yml"  
+  source           = "./prometheus/prometheus.yml"
+}
+
+resource "azurerm_storage_share_file" "prometheus_alert_rules" {
+  name             = "alert.rules"
+  storage_share_id = azurerm_storage_share.stg_files_prometheus.id
+  source           = "./prometheus/alert.rules"
 }
 
 resource "azurerm_storage_share" "stg_files_grafana" {
   name                 = "grafana"
   storage_account_name = azurerm_storage_account.main.name
-  quota = 10
+  quota                = 10
   depends_on           = [azurerm_storage_account.main]
 }
